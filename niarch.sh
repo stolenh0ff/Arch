3e3d00649584fc1e7485ed8c0d694c35b76f27a4
Benjamin Sepulveda <53204861+stolenh0ff@users.noreply.github.com>
2022-07-13 18:22:52 -0400

    Update niarch.sh

--- niarch.sh
+++ niarch.sh
@@ -1,1 +1,3 @@
-pacman -S sudo xorg lightdm lightdm-gtk-greeter qtile alacritty firefox pulseaudio pavucontrol pamixer 
+pacman -S xorg-xinit xorg-xrandr sudo xorg lightdm lightdm-gtk-greeter qtile alacritty firefox pulseaudio pavucontrol pamixer
+sudo systemctl enable lightdm
+reboot
