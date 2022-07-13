c4dd00e7ffcc0ec6de09de502405f90c94ea41c6
Benjamin Sepulveda <53204861+stolenh0ff@users.noreply.github.com>
2022-07-13 19:21:51 -0400

    Update niarch.sh

--- niarch.sh
+++ niarch.sh
@@ -1,6 +1,8 @@
 pacman -S xorg-xinit xorg-xrandr sudo xorg lightdm lightdm-gtk-greeter qtile alacritty firefox pulseaudio pavucontrol pamixer neofetch neovim ranger zsh wget
 sudo systemctl enable lightdm
 
+
+
 useradd -m username
 passwd username
 usermod -aG wheel,video,audio,storage username
