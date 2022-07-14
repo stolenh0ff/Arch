c159a18d958fc8455d9d6cf6d333f5a67004b559
stolenh0ff <alonsso008@gmail.com>
2022-07-14 00:58:02 -0400

    Simple ZSH and Oh_My_Zsh Installation for ARCH

---
+++ mc_server_setup.sh
@@ -0,0 +1,6 @@
+sudo pacman -S jre-openjdk
+cd /home/admin/
+mkdir MinecraftServer
+cd MinecraftServer
+wget https://launcher.mojang.com/v1/objects/e00c4052dac1d59a1188b2aa9d5a87113aaf1122/server.jar
+echo "Minecraft Server Downloaded"
---
+++ oh-my-zsh.sh
@@ -0,0 +1,2 @@
+sudo pacman -S wget zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
