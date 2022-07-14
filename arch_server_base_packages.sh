9315afeb732c13bd6048872b412e3dceba16003e
stolenh0ff <alonsso008@gmail.com>
2022-07-14 01:48:19 -0400

    Base Packages + ZSH for ARCH Server Installation

--- arch_server_base_packages.sh
+++ arch_server_base_packages.sh
@@ -1,5 +1,4 @@
 
-
 pacman -S jre-openjdk xorg-xinit xorg-xrandr sudo xorg lightdm lightdm-gtk-greeter qtile alacritty firefox pulseaudio pavucontrol pamixer neofetch neovim ranger zsh wget
 sudo systemctl enable lightdm
 
@@ -8,5 +7,8 @@ passwd admin
 usermod -aG wheel,video,audio,storage admin
 
 cp /opt/ARCH/sudoers /etc/sudoers
+
+sudo pacman -S wget zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
 reboot
