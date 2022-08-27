aaf9cdaea446b1887222ef6e4d69cc12fc413cb6
Benjamin Sepulveda <alonsso008@gmail.com>
2022-08-26 23:29:18 -0400

    Minor Changes

---
+++ Arch Desktop (Base)/Qtile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+pacman -S xorg-xinit xorg-xrandr sudo xorg lightdm lightdm-gtk-greeter qtile alacritty firefox pulseaudio pavucontrol pamixer neofetch neovim ranger zsh wget feh libmtp glib2 gvfs picom geeqie vlc wget zsh
+systemctl enable lightdm
--- DE/QtileWM.sh
+++
@@ -1,3 +0,0 @@
-#WIP
-sudo pacman -S qtile polybar
-systemctl enable lightdm     
