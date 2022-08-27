aa04248799d600d23862bac28ed76a26ef3c3c64
Benjamin Sepulveda <alonsso008@gmail.com>
2022-08-26 23:15:39 -0400

    Minor Changes

--- Utilities/aurhelper.sh
+++ Utilities/aurhelper.sh
@@ -1,9 +1,10 @@
 sudo pacman -S base-devel git
-mkdir ~/Git/ArchPackages
-cd ~/Git/ArchPackages
 sudo git clone https://aur.archlinux.org/yay-git.git
 echo "Type your username: "
 read username
 sudo chown -R "$username":"$username" yay-git/
 cd yay-git
 makepkg -si
+cd ..
+rm -rf yay-git
+sudo pacman -Scc
