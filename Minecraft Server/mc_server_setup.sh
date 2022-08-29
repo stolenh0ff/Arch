c6a23f745cadd060ddfe831984dabcd498890627
Benjamin Sepulveda <alonsso008@gmail.com>
2022-08-28 22:44:48 -0400

    a

--- Minecraft Server/mc_server_setup.sh
+++
@@ -1,7 +0,0 @@
-sudo pacman -S jre-openjdk
-echo "Moving minecraft_server.sh to /home/admin/MinecraftServer"
-mkdir /home/admin/MinecraftServer
-sudo mv minecraft_server.sh /home/admin/MinecraftServer
-cd /home/admin/MinecraftServer
-wget https://launcher.mojang.com/v1/objects/e00c4052dac1d59a1188b2aa9d5a87113aaf1122/server.jar
-echo "Minecraft Server Downloaded into >> /home/admin/MinecraftServer"
--- Minecraft Server/minecraft_server.sh
+++
@@ -1,2 +0,0 @@
-echo "Running Minecraft Server"
-java -Xmx8192M -Xms2048M -jar server.jar nogui
